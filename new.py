2bf97a2cceb66ff5144c4d5d044370cbd75a9a90
boddepallirakesh <141721486+boddepallirakesh@users.noreply.github.com>
2024-12-17 13:50:30 +0530

    Update new.py

--- new.py
+++ new.py
@@ -4,8 +4,11 @@ age = 25
 height = 5.9
 is_student = True
 
-Printing values
-print(f"Name: {name}")
-print(f"Age: {age}")
-print(f"Height: {height} meters")
-print(f"Is Student: {is_student}")
+
+
+
+
+
+
+
+
