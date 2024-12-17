2209489af4442065b45b0aaaad5bf8319dc8ca5a
boddepallirakesh <141721486+boddepallirakesh@users.noreply.github.com>
2024-12-17 13:46:59 +0530

    Create new.py

---
+++ new.py
@@ -0,0 +1,11 @@
+# Variable assignment
+name = "John"
+age = 25
+height = 5.9
+is_student = True
+
+# Printing values
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Height: {height} meters")
+print(f"Is Student: {is_student}")
