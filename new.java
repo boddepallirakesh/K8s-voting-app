06c63a5bd3356d9a9d57423af34c83cdc004a289
boddepallirakesh <141721486+boddepallirakesh@users.noreply.github.com>
2024-12-17 14:08:53 +0530

    Update new.java

--- new.java
+++ new.java
@@ -1,15 +1,18 @@
-public class VariablesExample {
+public class MathOperations {
     public static void main(String[] args) {
-        // Declare and initialize variables
-        String name = "Alice";
-        int age = 30;
-        double height = 5.7;
-        boolean isStudent = true;
+        int x = 10;
+        int y = 5;
 
-        // Print the values
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-        System.out.println("Height: " + height);
-        System.out.println("Is Student: " + isStudent);
+        int addition = x + y;
+        int subtraction = x - y;
+        int multiplication = x * y;
+        int division = x / y;
+        int modulus = x % y;
+
+        System.out.println("Addition: " + addition);
+        System.out.println("Subtraction: " + subtraction);
+        System.out.println("Multiplication: " + multiplication);
+        System.out.println("Division: " + division);
+        System.out.println("Modulus: " + modulus);
     }
 }
