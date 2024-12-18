4477bfb59c9ea09184628c976e8568b61602738f
boddepallirakesh <141721486+boddepallirakesh@users.noreply.github.com>
2024-12-18 01:44:14 -0800

    Delete jenkins.py

--- jenkins.py
+++
@@ -1,8 +0,0 @@
-# Working with lists
-fruits = ["apple", "banana", "cherry"]
-fruits.append("orange")  # Adding an item
-print(f"Updated list: {fruits}")
-
-# Iterating over a list
-for fruit in fruits:
-    print(f"I love {fruit}")
